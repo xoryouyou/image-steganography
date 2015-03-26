c5482568962433c6166600219298e4e0aa783382
kmw <kmw@b00t.me>
2015-03-26 22:29:30 +0100

    first commit

---
+++ src/encodeMask.py
@@ -0,0 +1,30 @@
+import argparse
+import Image
+
+from functions.binary import hideASCII
+from functions.binary import showASCII
+from functions.mask import encode
+from functions.mask import decode
+
+
+
+if __name__ == '__main__':
+    arg = argparse.ArgumentParser()
+    arg.add_argument('-m', '--message', required=True, action='store', help='Message image to be hidden in the target')
+    arg.add_argument('-t', '--target', required=True, action='store', help='Target image on which the mask shall be applied')
+    arg.add_argument('-o', '--output', required=True, action='store', help='Output image name')
+    args = vars(arg.parse_args())
+
+    img = encode(Image.open(args['message']), Image.open(args['target']))
+    img.save(args['output'])
+
+
+
+
+    #hidden = hideASCII("Hello World!")
+    #showASCII(hidden)
+
+    #img = hide(Image.open("../img/message.png"), Image.open("../img/background.png"))
+    #decoded = decode(img)
+    #decoded.show()
+    #img.save("../img/output2.png")
---
+++ src/functions/binary.py
@@ -0,0 +1,66 @@
+import Image
+import random
+
+def binary(message):
+  string = ""
+
+  for char in message:
+    string += (bin(ord(char))[2:])
+  string += '10101010'
+
+  print("Encoded:%s" % string)
+  for char in string:
+    yield int(char)
+
+  while True:
+    yield(int(random.randrange(1)))
+
+
+
+def setbit(byte, bit):
+  return (byte | bit) if bit else (byte & 0xFE)
+
+
+def hideASCII(message, img):
+
+  stream = binary(message)
+  w, h = img.size
+
+  for x in range(w):
+    for y in range(h):
+
+      r, g, b = img.getpixel((x, y))
+
+      r = setbit(r, stream.next())
+      g = setbit(g, stream.next())
+      b = setbit(b, stream.next())
+
+      img.putpixel((x, y), (r,g,b))
+
+  return img
+
+
+def showASCII(image):
+  #branch here for image type ,path, etc
+  message = ""
+
+  w, h = image.size
+
+  for x in range(w):
+    for y in range(h):
+      r, g, b = image.getpixel((x, y))
+
+      message += str(r & 1) # get lsb
+      message += str(g & 1) # get lsb
+      message += str(b & 1) # get lsb
+
+
+  offset = message.find("10101010")
+
+  print("Offset:%i" % offset)
+  message = message[:offset+8]
+  print("Decoded:%s" % message)
+
+  # chr(0b1101100)
+
+
