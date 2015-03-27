0746df897933ea110ef87f170074dea513a405ac
xoryouyou <till.von.ahnen@ubimake.com>
2015-03-27 17:01:50 +0100

    Fixed missing parameter

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@
 echo "Hiding mask image"
 python ./src/encodeMask.py -m img/message.png -t img/background.png -o out.png
 echo "Revealing mask image"
-python ./src/decodeMask.py -o out.png
+python ./src/decodeMask.py -i out.png
 echo "Hiding string"
 python ./src/encodeString.py -m 'this is a test'  -t img/background.png -o out2.png
 echo "Revealing string"
